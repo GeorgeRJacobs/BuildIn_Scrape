2b0364d56d30f3eade53722a14bf01feeb4aced9
GeorgeRJacobs <george.richard.jacobs@gmail.com>
2020-08-05 22:30:48 -0600

    Have decent code together to begin scraping.

--- src/playing_around/1_exploratory_code.py
+++ src/playing_around/1_exploratory_code.py
@@ -1,16 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
+import urllib
+import json
+import pandas as pd
 
 
 # Let's request the first page
 page1 = requests.get('https://www.builtinchicago.org/companies?status=all')
-page1
+urllib.parse.urlparse(page1.url)
 # Page 1
 soup = BeautifulSoup(page1.content, 'html.parser')
 
 # We need to find:
 # class = 'open-jobs'
 job_hrefs = soup.find_all("div", {"class": "open-jobs"})
+job_links = []
+base_url = urllib.parse.urlparse(page1.url)
+base_url.netloc
 for job in job_hrefs:
-    print(job.find('a')['href'])
+    job_links.append(base_url.netloc + job.find('a')['href'])
+job_links = [x.replace('/jobs', '') for x in job_links]
 #
+
+
+## Playing w/ page source
+company = requests.get('https://www.builtinchicago.org/company/marketing-store')
+company = BeautifulSoup(company.text, 'html.parser')
+company_data = company.find_all('script')
+for val in company_data:
+    if 'context' in val.string:
+        data_we_want = val.string
+        print(val.string)
+
+data = json.loads(data_we_want)
+data = data['@graph']
+t = {
+    'Company': [data[0]['name']],
+    'Number of Employees': [data[0]['numberOfEmployees']['name']],
+    'Company URL':  [data[0]['url']],
+    "Physical Address": [data[0]['address']["streetAddress"]],
+    'City': [data[0]['address']["addressLocality"]],
+    'State': [data[0]['address']['addressRegion']],
+    'ZIP': [data[0]['address']['postalCode']]
+}
+
--- src/scrapers/searchpage.py
+++ src/scrapers/searchpage.py
@@ -2,13 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import urllib
 import re
+import json
 
 
-class SearchPage:
+class Crawl:
     def __init__(self, search_start_url):
         self.search_start_url = search_start_url
         self.resource_name = None
         self.current_max_page_value = None
+        self.current_page = 0
+        self.base_url = urllib.parse.urlparse(search_start_url)
 
     def get_pagination_code(self):
         """
@@ -30,10 +33,58 @@ class SearchPage:
         self.current_max_page_value = max_val
         print(f'Max Page value: {self.current_max_value_page}')
 
+    def scrape_results_page(self):
+        """
+        After setting the pagination value, we need to actually grab the relevant company data. So that means grabbing
+        The Hrefs.
+        """
+        if self.current_page == 0:
+            page = requests.get('https://www.builtinchicago.org/companies?status=all')
+            soup = BeautifulSoup(page.content, 'html.parser')
+            job_hrefs = soup.find_all("div", {"class": "open-jobs"})
+            job_links = []
+            for job in job_hrefs:
+                job_links.append(self.base_url.netloc + job.find('a')['href'])
+            job_links = [x.str.replace('/jobs', '') for x in job_links]
+
+            # Run Data Fetch
+            for org in job_links:
+                self.scrape_company_page(org)
+        else:
+            pass
+
+    def scrape_company_page(self, organization):
+        """
+        Takes in the company url page and scrapes the data we need from it.
+        :param organization:
+        :return:
+        """
+        company = requests.get(organization)
+        company = BeautifulSoup(company.text, 'html.parser')
+        # Find All script sections containing JSON
+        company_data = company.find_all('script')
+        # Find specific JSON we want
+        for val in company_data:
+            if '@context' in val.string:
+                data_we_want = val.string
+        # Create Dictionary Value
+        data = json.loads(data_we_want)
+        data = {
+            'Company': [data[0]['name']],
+            'Number of Employees': [data[0]['numberOfEmployees']['name']],
+            'Company URL': [data[0]['url']],
+            "Physical Address": [data[0]['address']["streetAddress"]],
+            'City': [data[0]['address']["addressLocality"]],
+            'State': [data[0]['address']['addressRegion']],
+            'ZIP': [data[0]['address']['postalCode']]
+        }
 
 
-if __name__=="__main__":
-    page = SearchPage('https://www.builtinchicago.org/companies?status=all')
+
+
+
+if __name__ == "__main__":
+    page = Crawl('https://www.builtinchicago.org/companies?status=all')
     page.get_pagination_code()
     # search = requests.get('https://www.builtinchicago.org/companies?status=all&page=14')
     # # Parse
